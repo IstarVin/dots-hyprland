9bec3cd2b35b7a315eabe6c618b454e89c76ef80
IstarVin <ajdeleverio20@gmail.com>
2024-07-06 13:21:54 +0800

    basta

--- .config/ags/modules/sideright/centermodules/rogconfigure.js
+++ .config/ags/modules/sideright/centermodules/rogconfigure.js
@@ -1,46 +1,46 @@
 import Widget from 'resource:///com/github/Aylur/ags/widget.js';
-const { Box, Button, Entry, Icon, Label, Revealer, Scrollable, Slider, Stack, Overlay } = Widget;
-
+import { ConfigSpinButton } from '../../.commonwidgets/configwidgets.js';
+import * as Utils from 'resource:///com/github/Aylur/ags/utils.js';
+const { Box, Scrollable } = Widget;
+const { exec } = Utils;
 
 export default (props) => {
-    const ConfigSection = ({ name, children }) => Box({
-        vertical: true,
-        className: 'spacing-v-5',
-        children: [
-            Label({
-                hpack: 'center',
-                className: 'txt txt-large margin-left-10',
-                label: name,
-            }),
-            Box({
-                className: 'margin-left-10 margin-right-10',
-                vertical: true,
-                children: children,
-            })
-        ]
-    })
 
-    const mainContent = Scrollable({
-        vexpand: true,
-        child: Box({
-            vertical: true,
-            className: 'spacing-v-10',
-            children: [
-                ConfigSection({
-                    name: "ROG",
-                    children: []
-                })
-            ]
-        })
+  const mainContent = Scrollable({
+    vexpand: true,
+    child: Box({
+      vertical: true,
+      className: 'spacing-v-10',
+      children: [
+        ConfigSpinButton({
+          icon: "charger",
+          name: "Charge Limit",
+          desc: "Battery stops charging when reach limit. (20-100)",
+          initValue: Number(exec(`bash -c "cat /etc/asusd/asusd.ron | grep -oP 'charge_control_end_threshold: \K\d+' | cut -d',' -f1"`)),
+          step: 1, minValue: 20, maxValue: 100,
+          onChange: (_, newVal) => {
+            Utils.execAsync(`asusctl -c ${newVal}`).catch(print)
+          }
+        }),
+        // ConfigSection({
+        //     name: "Asusctl",
+        //     children: []
+        // }),
+        // ConfigSection({
+        //     name: "Supergfx",
+        //     children: []
+        // })
+      ]
     })
+  })
 
-    return Box({
-        ...props,
-        className: 'spacing-v-5',
-        vertical: true,
-        children: [
-            mainContent,
-            // footNote,
-        ]
-    })
+  return Box({
+    ...props,
+    className: 'spacing-v-5',
+    vertical: true,
+    children: [
+      mainContent,
+      // footNote,
+    ]
+  })
 }
