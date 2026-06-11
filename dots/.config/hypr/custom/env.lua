247e390e6622ff72564dbedca5f241d49a56be10
IstarVin <ajdeleverio20@gmail.com>
2026-06-12 01:45:31 +0800

    refactor: add ADW_DISABLE_PORTAL environment variable to env.lua

--- dots/.config/hypr/custom/env.lua
+++ dots/.config/hypr/custom/env.lua
@@ -1,3 +1,4 @@
 hl.env("ANDROID_HOME", "/home/aj/Android/Sdk")
 hl.env("NDK_HOME", "/home/aj/Android/Sdk/ndk/29.0.14206865")
 hl.env("WLR_DRM_DEVICES", "$HOME/.config/hypr/card")
+hl.env("ADW_DISABLE_PORTAL", "1")
